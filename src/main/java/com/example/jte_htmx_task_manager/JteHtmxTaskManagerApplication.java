173d08492fd3152bc2ee59dba7921bc040f9bf7c
mrdevsam <mdshoykotislam05@gmail.com>
2024-10-16 18:54:16 +0600

    initial commit

---
+++ src/main/java/com/example/jte_htmx_task_manager/JteHtmxTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jte_htmx_task_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JteHtmxTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JteHtmxTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jte_htmx_task_manager/JteHtmxTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jte_htmx_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JteHtmxTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
